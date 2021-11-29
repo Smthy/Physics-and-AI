b81f3b694076818a6ec6c4f3e619be090c154482
Alex Smith <Alexsmithgamedev@gmail.com>
2021-11-29 15:09:32 +0000

    Physics System

--- 8503 Release - 2021/CSC8503/CSC8503Common/PhysicsSystem.cpp
+++ 8503 Release - 2021/CSC8503/CSC8503Common/PhysicsSystem.cpp
@@ -233,7 +233,30 @@ based on any forces that have been accumulated in the objects during
 the course of the previous game frame.
 */
 void PhysicsSystem::IntegrateAccel(float dt) {
+	std::vector<GameObject*>::const_iterator first;
+	std::vector<GameObject*>::const_iterator last;
+	gameWorld.GetObjectIterators(first, last);
 
+
+	for (auto i = first; i != last; i++) {
+		PhysicsObject* object = (*i)->GetPhysicsObject();
+		if (object == nullptr) {
+			continue;
+		}
+
+		float inverseMass = object->GetInverseMass();
+
+		Vector3 linearVel = object->GetLinearVelocity();
+		Vector3 force = object->GetForce();
+		Vector3 accel = force * inverseMass;
+
+		if (applyGravity && inverseMass > 0) {
+			accel += gravity;
+		}
+
+		linearVel += accel * dt;
+		object->SetLinearVelocity(linearVel);
+	}
 }
 /*
 This function integrates linear and angular velocity into
@@ -242,6 +265,27 @@ throughout a physics update, to slowly move the objects through
 the world, looking for collisions.
 */
 void PhysicsSystem::IntegrateVelocity(float dt) {
+	std::vector<GameObject*>::const_iterator first;
+	std::vector<GameObject*>::const_iterator last;
+	gameWorld.GetObjectIterators(first, last);
+	float frameLinearDamping = 1.0f - (0.4f * dt);
+
+	for (auto i = first; i != last; i++) {
+		PhysicsObject* object = (*i)->GetPhysicsObject();
+		if (object == nullptr) {
+			continue;
+		}
+
+		Transform& transform = (*i)->GetTransform();
+
+		Vector3 position = transform.GetPosition();
+		Vector3 linearVel = object->GetLinearVelocity();
+		position += linearVel * dt;
+		transform.SetPosition(position);
+
+		linearVel = linearVel * frameLinearDamping;
+		object->SetLinearVelocity(linearVel);
+	}
 
 }
 
--- 8503 Release - 2021/CSC8503/GameTech/TutorialGame.cpp
+++ 8503 Release - 2021/CSC8503/GameTech/TutorialGame.cpp
@@ -553,5 +553,18 @@ added linear motion into our physics system. After the second tutorial, objects 
 line - after the third, they'll be able to twist under torque aswell.
 */
 void TutorialGame::MoveSelectedObject() {
+	renderer->DrawString("Click Force" + std::to_string(forceMagnitude), Vector2(10, 20));
+	forceMagnitude += Window::GetMouse()->GetWheelMovement() * 10000.0f;
 
+	if (!selectionObject) {
+		return;
+	}
+
+	if (Window::GetMouse()->ButtonPressed(NCL::MouseButtons::RIGHT)) {
+		Ray ray = CollisionDetection::BuildRayFromMouse(*world->GetMainCamera());
+		RayCollision closestCollision;
+		if (world->Raycast(ray, closestCollision, true)) {
+			selectionObject->GetPhysicsObject()->AddForce(ray.GetDirection() * forceMagnitude);
+		}
+	}
 }
